3d020215e53394b00400f699237462e897cd0177
Jiang-MingYi <1091759388@qq.com>
2023-04-16 16:54:21 +0800

    添加使用说明，修正服务器宕机时无法继续发送购买请求的问题

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 # now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
-TargetTime = "2023-03-05 17:00:00.00000000"     #设置抢购时间
+TargetTime = "2023-04-16 16:39:00.00000000"     #设置抢购时间
 WebDriver = webdriver.Chrome()
 WebDriver.get("https://show.bilibili.com/platform/detail.html?id=71212&from=pc_ticketlist")   #输入目标购买页面
 time.sleep(1)
@@ -14,13 +14,15 @@ WebDriver.find_element(By.CLASS_NAME, "nav-header-register").click()
 print("请在10s内登录")
 time.sleep(10)
 
-try:
-    WebDriver.find_element(By.CLASS_NAME, "product-buy")
-    print("确认此页面为预购页面")
-    time.sleep(3)
-except:
-    print("此页面未发现预购按钮")
-    time.sleep(3)
+# if(WebDriver.find_element(By.CLASS_NAME, "product-buy")):
+#     print("确认此页面为预购页面")
+#     time.sleep(3)
+# elif(WebDriver.find_element(By.CLASS_NAME, "product-buy.enable")):
+#     print("确认此页面为直接购买页面")
+#     time.sleep(3)
+# else:
+#     print("此页面未发现预购按钮")
+#     time.sleep(3)
 
 # while True:
 #     try:
@@ -30,11 +32,14 @@ except:
 #     except:
 #         print("无购买按钮")
 
-
 while True:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     print(now + "     " + TargetTime)
     if now >= TargetTime:
+        WebDriver.refresh()
+        break
+
+while True:
         try:
             WebDriver.find_element(By.CLASS_NAME, "product-buy.enable").click()
             # time.sleep(5)
@@ -46,7 +51,7 @@ while True:
         try:
             WebDriver.find_element(By.CLASS_NAME, "confirm-paybtn.active").click()
             print("订单创建完成，请在一分钟内付款")
-            time.sleep(60)
+            # time.sleep(60)
         except:
             print("无法点击创建订单")
 
