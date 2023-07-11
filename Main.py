0a536f4a463263ee400331249c5262e1837ee294
Jiang-MingYi <1091759388@qq.com>
2023-07-12 00:14:18 +0800

    增加场次价格选择功能和headless游览器模式，修复了开始抢票之后因刷新页面而造成场次价格选项混乱的bug

--- Main.py
+++ Main.py
@@ -1,52 +1,69 @@
 import datetime
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+import cv2
 
-# now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+def Init():
+    ch_options = Options()
+    ch_options.add_argument("--headless")
+    global TargetTime
+    TargetTime = "2023-07-12 23:00:00.00000000"  # 设置抢购时间
 
-TargetTime = "2023-04-16 16:39:00.00000000"     #设置抢购时间
-WebDriver = webdriver.Chrome()
-WebDriver.get("https://show.bilibili.com/platform/detail.html?id=71212&from=pc_ticketlist")   #输入目标购买页面
-time.sleep(1)
-print("进入购票页面成功")
-WebDriver.find_element(By.CLASS_NAME, "nav-header-register").click()
-print("请在10s内登录")
-time.sleep(10)
+    global WebDriver
+    # WebDriver = webdriver.Chrome(chrome_options=ch_options)         #使用headless游览器，速度更快
+    WebDriver = webdriver.Chrome()  # 使用可视化游览器
+    WebDriver.get("https://show.bilibili.com/platform/detail.html?id=73710&from=pc_ticketlist")  # 输入目标购买页面
 
-# if(WebDriver.find_element(By.CLASS_NAME, "product-buy")):
-#     print("确认此页面为预购页面")
-#     time.sleep(3)
-# elif(WebDriver.find_element(By.CLASS_NAME, "product-buy.enable")):
-#     print("确认此页面为直接购买页面")
-#     time.sleep(3)
-# else:
-#     print("此页面未发现预购按钮")
-#     time.sleep(3)
+    global Price, Session
+    Session = '1'  # 场次设置：修改引号内部的数字，数字对应第选项的序号，选项序号从左到右从1开始依次排列
+    Price = '1'  # 加个设置：设置方法与场次设置一样
 
-# while True:
-#     try:
-#         WebDriver.find_element(By.CLASS_NAME, "product-buy.enable")
-#         print("找到购买按钮")
-#         break
-#     except:
-#         print("无购买按钮")
+    time.sleep(1)
+    print("进入购票页面成功")
 
-while True:
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    print(now + "     " + TargetTime)
-    if now >= TargetTime:
-        WebDriver.refresh()
-        break
+    WebDriver.find_element(By.CLASS_NAME, "nav-header-register").click()
+    time.sleep(1)
 
-while True:
+    print("登录完成后关闭qrimg页面")
+    WebDriver.save_screenshot('./QRcode.png')
+    qrimg = cv2.imread('./QRcode.png')
+    cv2.imshow("qrimg", qrimg)
+    key = cv2.waitKey(0)
+    WebDriver.find_element(By.CLASS_NAME, "bili-mini-close").click()
+
+    # print("请在10s内登录")
+    # time.sleep(10)
+
+def Select():
+    while True:
+        try:
+            WebDriver.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div[2]/div[4]/ul[1]/li[2]/div[' + Session + ']').click()
+            WebDriver.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div[2]/div[4]/ul[2]/li[2]/div[' + Price + ']').click()
+            break
+        except:
+            print("等待刷新")
+
+
+def Wait():
+    while True:
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        print(now + "     " + TargetTime)
+        if now >= TargetTime:
+            WebDriver.refresh()
+            Select()
+            break
+
+def Buy():
+    print("test1")
+    while True:
         try:
             WebDriver.find_element(By.CLASS_NAME, "product-buy.enable").click()
             # time.sleep(5)
             print("进入购买页面成功")
         except:
             print("无法点击购买")
-
 
         try:
             WebDriver.find_element(By.CLASS_NAME, "confirm-paybtn.active").click()
@@ -56,7 +73,10 @@ while True:
             print("无法点击创建订单")
 
 
-
-
+if __name__ == '__main__':
+    Init()
+    Select()
+    Wait()
+    Buy()
 
 
--- README.md
+++ README.md
@@ -1,25 +1,42 @@
 # BilibiliTicketRushBuy
- bilibili会员购抢票脚本，使用时需自行设置目标抢购页面与抢购时间
+ bilibili会员购抢票脚本，使用时需自行设置相关参数
+## 参数设置
+抢购时间设置：Main.py第12行
 
-目标抢购页面设置：Main.py第10行
+目标抢购页面设置：Main.py第17行
 
-抢购时间设置：Main.py第8行
+选择是否使用headless游览器：Main.py第15、16行
+
+场次选择：Main.py第20行
+
+价格选择：Main.py第21行
 ## 环境
 python == 3.10
 
+Google Chrome == 正式版
+
 ## 使用流程
-1.按照上述操作设置好目标抢购页面与抢购时间
+1.按照上述操作设置好相关参数
 
 2.在项目所在路径打开cmd
 
 3.依次输入以下指令并运行：
 ```
-pip install selenium
+pip install -r requirements.txt
 python Main.py
 ```
-4.若自动打开网页，则程序正常运行，请等待购买页面自动跳转出现bilibili账号登录页面，并在10s内扫码登录，随后程序将自动运行，无需额外操作。
+4.若有正常输出，则程序正常运行，请等待出现名为qrimg的窗口（其中内容为bilibili账号登录页面截图），待扫码登录后关闭qrimg窗口，随后程序将自动运行，无需额外操作。
+## TODO
+1.多线程运行
 
+2.可视化页面
 ## 注意事项
+若pip安装出现网络问题，可尝试使用以下命令进行安装
+```
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+```
+
+
 使用程序前，请保证网络畅通和电脑时间为北京时间。
 
 本程序仅适用与bilibili会员购抢票，一切因本项目引起的经济问题，本项目不负任何责任。并且此项目无法保证100%抢票成功，请将此项目作备用手段而非唯一手段使用
